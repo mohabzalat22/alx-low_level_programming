c4f71ca0253160ac0b83252e723c342a88292a6c
mohabzalat22 <mohabzalat20@gmail.com>
2023-07-14 08:20:26 -0700

    new commit !

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,16 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - main block
  * Return: 0
  */
 int main(void)
 {
-char c = 'a';
+	char c = 'a';
 
-while (c < 'Z')
-{
-	putchar(c);
-}
-putchar('\n');
-return (0);
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	c = 'A';
+
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
 }
