584c1e9d3dfb2f98955f898a6c83dd3d7aa47ac5
mohabzalat22 <mohabzalat20@gmail.com>
2023-07-18 13:28:36 -0700

    new commit !

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,13 @@ void print_alphabet(void)
 {
 int i;
 int l = 0;
-while (l < 10)
-{
-for (i = 'a'; i <= 'z'; i++)
-{
-_putchar (i);
-}
-_putchar ('\n');
-}
-l++;
+	while (l < 10)
+	{
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	_putchar (i);
+	}
+	_putchar ('\n');
+	l++;
+	}
 }
