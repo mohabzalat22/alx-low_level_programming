39e9505d1148cfb6250b031b77b122e825858a0f
mohabzalat22 <mohabzalat20@gmail.com>
2023-07-18 13:22:26 -0700

    new commit !

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*print_alphabet - function that print alphabet
+*it prints in lowercase
+*Return: no return
+*/
+void print_alphabet(void)
+{
+int i;
+while (l < 10)
+{
+for (i = 'a';i <= 'z'; i++)
+{
+_putchar (i);
+}
+_putchar ('\n');
+}
+}
