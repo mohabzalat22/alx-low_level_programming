ee925d4f8a977fad634e62c3ebb7959b9a3ed533
mohabzalat22 <mohabzalat20@gmail.com>
2023-07-18 12:47:33 -0700

    new commit !

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include<main.h>
+?
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+char s[8]="_putchar";
+for (int i = 0; i<8; i++)
+{
+_putchar(s[i]);
+}
+__putchar('\n');
+return (0);
+}
